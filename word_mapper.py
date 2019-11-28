922a4a8420ff5d5f1c0e83d299b165d2a67a9007
Misgana Bayetta <misgana.bayetta@gmail.com>
2019-11-28 18:04:48 +0800

    Integrate edit distance

--- word_mapper.py
+++ word_mapper.py
@@ -1,9 +1,19 @@
 # encoding=utf8
+import operator
 import sys
+import time
 reload(sys)
 sys.setdefaultencoding('utf8')
 from keymap import *
-#from trie_dict import *
+from trie_dict import *
+
+
+trie = TrieNode()
+
+print("Loading dictionary...")
+ti = time.time()
+load_dict(trie)
+print("Loaded dict in {} sec.".format(time.time() - ti))
 
 # This may end up doing more complex tokenizations.
 def tokenize(sentence):
@@ -22,7 +32,7 @@ def generate_am_words(en_word):
         key = en_word[0: i+1]
         if key in KEY_MAP:
             first_chars = KEY_MAP[key]
-            print("{} matched {}".format(key, unicode("".join(first_chars))))
+            #print("{} matched {}".format(key, unicode("".join(first_chars))))
             # If we don't have any more english substring to match for.
             if len(en_word)-1 < i+1:
                 for char in first_chars:
@@ -58,6 +68,38 @@ def generate_sentence(en_sent):
         am_words = generate_am_words(word)
         sentence_words.append(am_words)
 
+    # sort words by their edit distance.
+    # for now if word is not found, lets assume it is unknown.
+    scored_sentence_words = []
+    for words in sentence_words:
+        scored_words = []
+        found = False
+        for word in words:
+            print("Calculating the edit distance for {}".format(word))
+            max_cost = 1 if len(word) <= 4 else 2
+            results = search(trie, word, max_cost)
+            if len(results) >= 1:
+                results.sort(key=operator.itemgetter(1))
+                # Choose the word with the least cost.
+                print(" Found a possible match {} with score {}".format(str(unicode(results[0][0])), results[0][1]))
+                scored_words.append(results[0])
+                found = True
+            else:
+                print("{} is unkown to our dictionary".format(word))
+        if not found:
+            print("no single word was a match to {}. Exiting.".format(words))
+            return []
+        scored_words.sort(key=operator.itemgetter(1))
+        scored_sentence_words.append(scored_words)
+
+    sentences = []
+    sent = ""
+    for i in range(0, len(scored_sentence_words)):
+        print("Chose word {} with cost {}".format(str(unicode(scored_sentence_words[i][0][0])), scored_sentence_words[i][0][1]))
+        sent += (str(unicode(scored_sentence_words[i][0][0])) + " ")
+    sentences.append(sent)
+    # This is is a very costly and perhaps not that useful strategy.
+    '''
     combs = _cartesian(sentence_words)
     sentences = []
     for i in range(0, len(combs)):
@@ -65,8 +107,7 @@ def generate_sentence(en_sent):
         for j in range(0, len(combs[i])):
            sent += combs[i][j]+ " "
         sentences.append(sent)
-
-    # TODO Use edit distance for scoring
+    '''
     return sentences
 '''
 en_word = "lili"
@@ -81,3 +122,4 @@ for result in _cartesian([['x', 'y', 'z'], ['a', 'b', 'c'], ['f', 'j', 'k']]):
 
 for sent in generate_sentence("lili konjo lij nat"):
     print(sent)
+
